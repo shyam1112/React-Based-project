14c972dd92bc3f5ce4135550e7cd254349ffe92b
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-08-18 19:28:14 +0530

    a

--- src/Navbarr.js
+++ src/Navbarr.js
@@ -1,20 +1,26 @@
 
-import { Link } from 'react-router-dom';
-import About from './About';
-function Navbarr() {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
   return (
-    <div>
-        <div className="topnav">
-          <Link to="/" className="active">Home</Link>
-          <Link to="/About">About</Link>
-          <Link to="/User">User</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/User/ab">AB</Link>
-          <Link to="/User/cd">CD</Link>
-        </div>
-        
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="user/ab">AB</Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbarr;
+export default BasicExample;
