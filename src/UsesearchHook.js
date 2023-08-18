8b7dc7005e422a7902c8f2bf09def054e488ec53
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-08-18 19:59:13 +0530

    a

--- src/UsesearchHook.js
+++ src/UsesearchHook.js
@@ -2,10 +2,11 @@ import { useSearchParams } from "react-router-dom";
 function UsesearchHook(){
     const [searchparam,setSearchparam]=useSearchParams();
     const age=searchparam.get('age');
+    const city=searchparam.get('text');
     return(
         <div>
             <h2>Age : {age}</h2>
-            <h2>city :</h2>
+            <h2>city : {city}</h2>
             <input type="text" onChange={(e)=>setSearchparam({text:e.target.value,age})}/>
             <button onClick={()=>setSearchparam({age:40})}>Click</button>
         </div>
